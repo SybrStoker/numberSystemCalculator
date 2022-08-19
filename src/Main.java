b0ccef58f5255cf57861393b3cfe4d24fdc860c8
SybrStoker <gorindan2@gmail.com>
2022-08-19 16:49:54 +0700

    The common plan of prog

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This program helps u to operate with another numeral systems.");
+
+        //language switch
+        System.out.println("Choose ur language:" +
+                "\n1.English" +
+                "\n2.German");
+
+        //operation switch
+        System.out.println("Available operations:" +
+                "\n1. Translation" +
+                "\n2. Common math");
+
+        //first menu button
+        System.out.println("1.1 Translate a number from a necessary ns to 10th ns" +
+                "\n1.2 Translate a 10th number to a necessary ns" +
+                "\n1.3 Back");
+
+        //second menu button
+        System.out.println("2.1 Addition" +
+                "\n2.2 Subtraction" +
+                "\n2.3 Multiplication" +
+                "\n2.4 Division" +
+                "\n2.5 Back");
+    }
+}
